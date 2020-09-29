38c3abfc15f590d7be406b7cf44edbec176b30c7
Billy Mosis <edo.billy@gmail.com>
2020-09-29 22:25:56 +0700

    Add tracking id for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-172466380-1`,
       },
     },
     `gatsby-plugin-feed`,
